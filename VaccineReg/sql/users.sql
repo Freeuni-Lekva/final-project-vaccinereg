6a21d44c1cd6e925280e1b48dcc5a1dc6e927791
aksov19 <aksov19@freeuni.edu.ge>
2021-08-03 22:00:20 +0400

    added user database

---
+++ VaccineReg/sql/users.sql
@@ -0,0 +1,24 @@
+-- TODO: change to your db
+USE uni_oop_project;
+
+DROP TABLE IF EXISTS users;
+ -- remove table if it already exists and start from scratch
+
+CREATE TABLE users (
+	private_num 	BIGINT 		PRIMARY KEY,
+    name 			CHAR(64) 	NOT NULL,
+    last_name 		CHAR(64) 	NOT NULL,
+    gender 			CHAR(16)	NOT NULL,
+    age 			INT 		NOT NULL,
+    email 			CHAR(128) 	NOT NULL UNIQUE,
+    password 		CHAR(64) 	NOT NULL,
+    is_admin		BOOLEAN		NOT NULL
+    
+    #vaccination_id INT 		FOREIGN KEY
+);
+
+INSERT INTO users VALUES
+	(111231455, "Archil", "Ksovreli", "male", 20, "aksov19@freeuni.edu.ge", "achiachi", true),
+    (221231455, "name1", "lastname1", "female", 45, "test1@freeuni.edu.ge", "test1", false),
+    (291291495, "name2", "lastname2", "male", 27, "test2@gmail.com", "test2", false),
+    (221239999, "name3", "lastname3", "female", 21, "test3@gmail.com", "test3", false)

5c5dd59d50e9a2bdf0299b2326b67bd15272c392
aksov19 <aksov19@freeuni.edu.ge>
2021-08-03 23:15:21 +0400

    removed private info

--- VaccineReg/src/main/java/DB.java
+++ VaccineReg/src/main/java/DB.java
@@ -2,8 +2,8 @@
  * Static class that holds variables for connecting to the local MySQL database
  */
 public class DB {
-    public final static String username = "aksov19";
-    public final static String password = "achi2001";
-    public final static String server   = "localhost";
-    public final static String database = "uni_oop_project";
+    public final static String username = "";
+    public final static String password = "";
+    public final static String server   = "";
+    public final static String database = "";
 }

facbfdb6d6257fcedb9bbd86b4243837cbf305aa
aksov19 <aksov19@freeuni.edu.ge>
2021-08-05 20:33:33 +0400

    created main package for classes

---
+++ VaccineReg/src/main/java/vaccinereg/DB.java
@@ -0,0 +1,11 @@
+package vaccinereg;
+
+/**
+ * Static class that holds variables for connecting to the local MySQL database
+ */
+public class DB {
+    public final static String username = "";
+    public final static String password = "";
+    public final static String server   = "";
+    public final static String database = "";
+}

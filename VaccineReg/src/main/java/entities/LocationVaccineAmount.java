637cb37b0ef11ed2cda7b30a275a2b75d4d3a552
giorgi <gpavl19@freeuni.edu.ge>
2021-08-08 23:12:24 +0400

    fixed minor mistakes and added entity classes

---
+++ VaccineReg/src/main/java/entities/LocationVaccineAmount.java
@@ -0,0 +1,66 @@
+package entities;
+
+public class LocationVaccineAmount {
+    private Long id;
+    private Long vaccineCenterId;
+    private String vaccineName;
+    private Integer amount;
+
+    public LocationVaccineAmount(Long id, Long vaccineCenterId, String vaccineName, Integer amount) {
+        this.id = id;
+        this.vaccineCenterId = vaccineCenterId;
+        this.vaccineName = vaccineName;
+        this.amount = amount;
+    }
+
+    public LocationVaccineAmount(Long vaccineCenterId, String vaccineName, Integer amount) {
+        this.vaccineCenterId = vaccineCenterId;
+        this.vaccineName = vaccineName;
+        this.amount = amount;
+    }
+
+    public LocationVaccineAmount() {
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setVaccineCenterId(Long vaccineCenterId) {
+        this.vaccineCenterId = vaccineCenterId;
+    }
+
+    public void setVaccineName(String vaccineName) {
+        this.vaccineName = vaccineName;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getVaccineCenterId() {
+        return vaccineCenterId;
+    }
+
+    public String getVaccineName() {
+        return vaccineName;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        LocationVaccineAmount l = (LocationVaccineAmount) o;
+        return (this.id.equals(l.getId()) && this.vaccineCenterId.equals(l.getVaccineCenterId()) && this.vaccineName.equals(l.getVaccineName()) && this.amount.equals(l.getAmount()));
+    }
+}
--- VaccineReg/src/main/java/entities/User.java
+++ VaccineReg/src/main/java/entities/User.java
@@ -1,41 +1,17 @@
-package vaccinereg;
+package entities;
 
 public class User {
-    private final long privateNum;
-    private final String name;
-    private final String lastName;
-    private final String gender;
-    private final int age;
-    private final String email;
-    private final String password;
-    private boolean isAdmin;
-    private int vaccinationCount;
-    // TODO: registration id
+    private long privateNum;
+    private String name;
+    private String lastName;
+    private String gender;
+    private int age;
+    private String email;
+    private String password;
+    private Boolean isAdmin;
+    private Long reservationId;
 
-    public User(long privateNum, String name,
-                String lastName, String gender,
-                int age, String email,
-                String password)
-    {
-        this.privateNum = privateNum;
-        this.name = name;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.age = age;
-        this.email = email;
-        this.password = password;
-
-        this.isAdmin = false;
-        this.vaccinationCount = 0;
-    }
-
-
-    public User(long privateNum, String name,
-                String lastName, String gender,
-                int age, String email,
-                String password, boolean isAdmin,
-                int vaccinationCount)
-    {
+    public User(long privateNum, String name, String lastName, String gender, int age, String email, String password, Boolean isAdmin, Long reservationId) {
         this.privateNum = privateNum;
         this.name = name;
         this.lastName = lastName;
@@ -44,9 +20,59 @@ public class User {
         this.email = email;
         this.password = password;
         this.isAdmin = isAdmin;
-        this.vaccinationCount = vaccinationCount;
+        this.reservationId = reservationId;
     }
 
+    public User(long privateNum, String name, String lastName, String gender, int age, String email, String password) {
+        this.privateNum = privateNum;
+        this.name = name;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = false;
+    }
+
+    public User() {
+
+    }
+
+    public void setPrivateNum(Long privateNum) {
+        this.privateNum = privateNum;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public void setReservationId(Long reservationId) {
+        this.reservationId = reservationId;
+    }
 
     public long getPrivateNum() {
         return privateNum;
@@ -60,14 +86,11 @@ public class User {
         return lastName;
     }
 
-    /**
-     * @return "m" for male, "f" for female
-     */
     public String getGender() {
         return gender;
     }
 
-    public int getAge() {
+    public Integer getAge() {
         return age;
     }
 
@@ -79,20 +102,12 @@ public class User {
         return password;
     }
 
-    public boolean isAdmin() {
+    public Boolean getAdmin() {
         return isAdmin;
     }
 
-    public void setAdmin(boolean admin) {
-        isAdmin = admin;
-    }
-
-    public int getVaccinationCount() {
-        return vaccinationCount;
-    }
-
-    public void setVaccinationCount(int vaccination_count) {
-        this.vaccinationCount = vaccination_count;
+    public Long getReservationId() {
+        return reservationId;
     }
 
     @Override
@@ -101,8 +116,8 @@ public class User {
         if (!(o instanceof User)) return false;
 
         User user = (User) o;
-        return privateNum == user.privateNum && age == user.age && isAdmin == user.isAdmin &&
-                name.equals(user.name) && lastName.equals(user.lastName) && gender.equals(user.gender) &&
-                email.equals(user.email) && password.equals(user.password) && vaccinationCount == user.vaccinationCount;
+        return (this.privateNum == user.getPrivateNum() && this.age == user.getAge() && this.isAdmin.equals(user.getAdmin()) &&
+                this.name.equals(user.getName()) && this.lastName.equals(user.getLastName()) && this.gender.equals(user.getGender()) &&
+                this.email.equals(user.getEmail()) && this.password.equals(user.getPassword()));
     }
 }

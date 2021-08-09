bbf15bc5c520d5449d982ab4bbe91f4a8f723095
TM <tato.maisuradze@gmail.com>
2021-08-09 23:22:44 +0400

    Removed personal info

--- VaccineReg/src/main/java/vaccinereg/DB.java
+++ VaccineReg/src/main/java/vaccinereg/DB.java
@@ -4,8 +4,8 @@ package vaccinereg;
  * Static class that holds variables for connecting to the local MySQL database
  */
 public class DB {
-    public final static String username = "tm";
-    public final static String password = "Root_123";
-    public final static String server   = "localhost:3306";
-    public final static String database = "uni_oop_project";
+    public final static String username = "";
+    public final static String password = "";
+    public final static String server   = "";
+    public final static String database = "";
 }

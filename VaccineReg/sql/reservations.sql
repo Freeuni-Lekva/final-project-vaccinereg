908e4ad1464af599db01bfd93316367915535019
xd-ai <vice2361@gmail.com>
2021-08-12 02:52:20 +0400

    replace TIMESTAMP with DATETIME

--- VaccineReg/sql/reservations.sql
+++ VaccineReg/sql/reservations.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS reservations;
 
 CREATE TABLE reservations (
     id BIGINT PRIMARY KEY,
-    start_time_one TIMESTAMP NOT NULL,
-    start_time_two TIMESTAMP NOT NULL,
+    start_time_one DATETIME NOT NULL,
+    start_time_two DATETIME NOT NULL,
     location_vaccine_amount_id BIGINT,
 
     FOREIGN KEY (location_vaccine_amount_id) REFERENCES location_vaccine_amounts(id)

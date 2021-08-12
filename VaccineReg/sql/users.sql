84b3a93269592bc828a4124f4fcfebe998727577
aksov19 <aksov19@freeuni.edu.ge>
2021-08-12 13:47:29 +0400

    removed outdated insert statement

--- VaccineReg/sql/users.sql
+++ VaccineReg/sql/users.sql
@@ -17,10 +17,4 @@ CREATE TABLE users (
 
     FOREIGN KEY (reservation_id) REFERENCES reservations(id)
 );
-
-INSERT INTO users VALUES
-	(111231455, "Archil", "Ksovreli", "m", 20, "aksov19@freeuni.edu.ge", "achiachi", true, 1),
-    (221231455, "name1", "lastname1", "f", 45, "test1@freeuni.edu.ge", "test1", false, 0),
-    (291291495, "name2", "lastname2", "m", 27, "test2@gmail.com", "test2", false, 0),
-    (221239999, "name3", "lastname3", "f", 21, "test3@gmail.com", "test3", false, 0);
     

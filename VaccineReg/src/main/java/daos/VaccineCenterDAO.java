6bbfda1cded1a3d913f6c7444b99631dee88cb5c
xd-ai <vice2361@gmail.com>
2021-08-14 20:32:47 +0400

    Added missing database connection close statements and try/catch blocks

--- VaccineReg/src/main/java/daos/VaccineCenterDAO.java
+++ VaccineReg/src/main/java/daos/VaccineCenterDAO.java
@@ -25,27 +25,35 @@ public class VaccineCenterDAO {
         } catch (Exception ignored) {}
     }
 
-    public void addVaccineCenter(VaccineCenter vaccineCenter) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("INSERT INTO " + tableName + " VALUES (?, ?, ?);");
-        stmt.setString(1 , vaccineCenter.getRegionName());
-        stmt.setString(2 , vaccineCenter.getCityName());
-        stmt.setString(3 , vaccineCenter.getDistrictName());
-        stmt.execute();
-        con.close();
+    public void addVaccineCenter(VaccineCenter vaccineCenter) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("INSERT INTO " + tableName + " VALUES (?, ?, ?);");
+            stmt.setString(1, vaccineCenter.getRegionName());
+            stmt.setString(2, vaccineCenter.getCityName());
+            stmt.setString(3, vaccineCenter.getDistrictName());
+            stmt.execute();
+            con.close();
+        } catch (Exception ignored) {
+
+        }
     }
 
-    public VaccineCenter getVaccineCenterById(Long id) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("SELECT * FROM " + tableName + " WHERE id = ?;");
-        stmt.setLong(1 , id);
-        ResultSet res = stmt.executeQuery();
-
-        if(!res.next()){
+    public VaccineCenter getVaccineCenterById(Long id) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM " + tableName + " WHERE id = ?;");
+            stmt.setLong(1, id);
+            ResultSet res = stmt.executeQuery();
+            if (!res.next()) {
+                return null;
+            }
+            VaccineCenter vaccineCenter = new VaccineCenter(res.getLong(1), res.getString(2), res.getString(3), res.getString(4));
+            con.close();
+            return vaccineCenter;
+        } catch (Exception ignored) {
             return null;
         }
-
-        return new VaccineCenter(res.getLong(1) , res.getString(2) , res.getString(3) , res.getString(4));
     }
 
     public List<VaccineCenter> getAllVaccineCenters() {
@@ -58,85 +66,116 @@ public class VaccineCenterDAO {
             while (res.next()) {
                 result.add(new VaccineCenter(res.getLong(1), res.getString(2), res.getString(3), res.getString(4)));
             }
-
+            con.close();
             return result;
         } catch (Exception ignored) {
             return null;
         }
     }
 
-    public List<VaccineCenter> getVaccineCenterByRegionName(String regionName) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("SELECT * FROM " + tableName + " WHERE region_name = ?;");
-        stmt.setString(1 , regionName);
-        ResultSet res = stmt.executeQuery();
+    public List<VaccineCenter> getVaccineCenterByRegionName(String regionName) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM " + tableName + " WHERE region_name = ?;");
+            stmt.setString(1, regionName);
+            ResultSet res = stmt.executeQuery();
 
-        List<VaccineCenter> result = new ArrayList<>();
-        while(res.next()){
-            result.add(new VaccineCenter(res.getLong(1) , res.getString(2) , res.getString(3) , res.getString(4)));
+            List<VaccineCenter> result = new ArrayList<>();
+            while (res.next()) {
+                result.add(new VaccineCenter(res.getLong(1), res.getString(2), res.getString(3), res.getString(4)));
+            }
+            con.close();
+            return result;
+        } catch (Exception ignored) {
+            return null;
         }
-        return result;
     }
 
-    public List<VaccineCenter> getVaccineCenterByCityName(String cityName) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("SELECT * FROM " + tableName + " WHERE city_name = ?;");
-        stmt.setString(1 , cityName);
-        ResultSet res = stmt.executeQuery();
+    public List<VaccineCenter> getVaccineCenterByCityName(String cityName) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM " + tableName + " WHERE city_name = ?;");
+            stmt.setString(1, cityName);
+            ResultSet res = stmt.executeQuery();
 
-        List<VaccineCenter> result = new ArrayList<>();
-        while(res.next()){
-            result.add(new VaccineCenter(res.getLong(1) , res.getString(2) , res.getString(3) , res.getString(4)));
+            List<VaccineCenter> result = new ArrayList<>();
+            while (res.next()) {
+                result.add(new VaccineCenter(res.getLong(1), res.getString(2), res.getString(3), res.getString(4)));
+            }
+            con.close();
+            return result;
+        } catch (Exception ignored) {
+            return null;
         }
-        return result;
     }
 
-    public List<VaccineCenter> getVaccineCenterByDistrictName(String districtName) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("SELECT * FROM " + tableName + " WHERE district_name = ?;");
-        stmt.setString(1 , districtName);
-        ResultSet res = stmt.executeQuery();
+    public List<VaccineCenter> getVaccineCenterByDistrictName(String districtName) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM " + tableName + " WHERE district_name = ?;");
+            stmt.setString(1, districtName);
+            ResultSet res = stmt.executeQuery();
 
-        List<VaccineCenter> result = new ArrayList<>();
-        while(res.next()){
-            result.add(new VaccineCenter(res.getLong(1) , res.getString(2) , res.getString(3) , res.getString(4)));
+            List<VaccineCenter> result = new ArrayList<>();
+            while (res.next()) {
+                result.add(new VaccineCenter(res.getLong(1), res.getString(2), res.getString(3), res.getString(4)));
+            }
+            con.close();
+            return result;
+        } catch (Exception ignored) {
+            return null;
         }
-        return result;
     }
 
-    public void deleteVaccineCenterById(Long id) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("DELETE FROM " + tableName + " WHERE id = ?;");
-        stmt.setLong(1 , id);
-        stmt.execute();
-        con.close();
+    public void deleteVaccineCenterById(Long id) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("DELETE FROM " + tableName + " WHERE id = ?;");
+            stmt.setLong(1, id);
+            stmt.execute();
+            con.close();
+        } catch (Exception ignored) {
+
+        }
     }
 
-    public void updateVaccineCenterRegionName(Long id , String regionName) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("UPDATE " + tableName + " SET region_name = ? WHERE id = ?;");
-        stmt.setString(1 , regionName);
-        stmt.setLong(2 , id);
-        stmt.execute();
-        con.close();
+    public void updateVaccineCenterRegionName(Long id , String regionName) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("UPDATE " + tableName + " SET region_name = ? WHERE id = ?;");
+            stmt.setString(1, regionName);
+            stmt.setLong(2, id);
+            stmt.execute();
+            con.close();
+        } catch (Exception ignored) {
+
+        }
     }
 
-    public void updateVaccineCenterCityName(Long id , String cityName) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("UPDATE " + tableName + " SET city_name = ? WHERE id = ?;");
-        stmt.setString(1 , cityName);
-        stmt.setLong(2 , id);
-        stmt.execute();
-        con.close();
+    public void updateVaccineCenterCityName(Long id , String cityName) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("UPDATE " + tableName + " SET city_name = ? WHERE id = ?;");
+            stmt.setString(1, cityName);
+            stmt.setLong(2, id);
+            stmt.execute();
+            con.close();
+        } catch (Exception ignored) {
+
+        }
     }
 
-    public void updateVaccineCenterDistrictName(Long id , String districtName) throws SQLException {
-        Connection con = ds.getConnection();
-        PreparedStatement stmt = con.prepareStatement("UPDATE " + tableName + " SET district_name = ? WHERE id = ?;");
-        stmt.setString(1 , districtName);
-        stmt.setLong(2 , id);
-        stmt.execute();
-        con.close();
+    public void updateVaccineCenterDistrictName(Long id , String districtName) {
+        try {
+            Connection con = ds.getConnection();
+            PreparedStatement stmt = con.prepareStatement("UPDATE " + tableName + " SET district_name = ? WHERE id = ?;");
+            stmt.setString(1, districtName);
+            stmt.setLong(2, id);
+            stmt.execute();
+            con.close();
+        } catch (Exception ignored) {
+
+        }
     }
 
 

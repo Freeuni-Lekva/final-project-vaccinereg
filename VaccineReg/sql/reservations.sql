a66d9746383814d0e2baa796dcedc0023157ba7f
xd-ai <vice2361@gmail.com>
2021-08-14 21:23:59 +0400

    Added a foreign key to the reservations table for the user id

--- VaccineReg/sql/reservations.sql
+++ VaccineReg/sql/reservations.sql
@@ -7,10 +7,12 @@ CREATE TABLE reservations (
     reservation_time DATETIME NOT NULL,
     vaccination_time DATETIME NOT NULL,
     location_vaccine_amount_id BIGINT,
+    user_id BIGINT,
 
-    FOREIGN KEY (location_vaccine_amount_id) REFERENCES location_vaccine_amounts(id)
+    FOREIGN KEY (location_vaccine_amount_id) REFERENCES location_vaccine_amounts(id),
+    FOREIGN KEY (user_id) REFERENCES users(private_num)
 );
 INSERT INTO reservations VALUES
-(1, '2021-08-13 16:55:00', '2021-08-13 16:55:00', 1),
-(2, '2021-08-13 16:52:00', '2021-08-13 16:52:00', 2),
-(3, '2021-08-13 12:42:00', '2021-08-13 12:42:00', 3);
+(1, '2021-08-13 16:55:00', '2021-08-13 16:55:00', 1, 111231455),
+(2, '2021-08-13 16:52:00', '2021-08-13 16:52:00', 2, 221231455),
+(3, '2021-08-13 12:42:00', '2021-08-13 12:42:00', 3, 291291495);

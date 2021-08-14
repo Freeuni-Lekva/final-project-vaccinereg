7c66f997b4dfbb7679a68f4c6c6b208627f67346
xd-ai <vice2361@gmail.com>
2021-08-14 21:24:45 +0400

    Adjusted Reservation class to match the updated table

--- VaccineReg/src/main/java/entities/Reservation.java
+++ VaccineReg/src/main/java/entities/Reservation.java
@@ -7,18 +7,21 @@ public class Reservation {
     private LocalDateTime vaccinationTime;
     private LocalDateTime reservationTime;
     private Long location_vaccine_amount_id;
+    private Long user_id;
 
-    public Reservation(Long id, LocalDateTime vaccinationTime, LocalDateTime reservationTime, Long location_vaccine_amount_id) {
+    public Reservation(Long id, LocalDateTime vaccinationTime, LocalDateTime reservationTime, Long location_vaccine_amount_id, Long user_id) {
         this.id = id;
         this.vaccinationTime = vaccinationTime;
         this.reservationTime = reservationTime;
         this.location_vaccine_amount_id = location_vaccine_amount_id;
+        this.user_id = user_id;
     }
 
-    public Reservation(LocalDateTime vaccinationTime, LocalDateTime reservationTime, Long location_vaccine_amount_id) {
+    public Reservation(LocalDateTime vaccinationTime, LocalDateTime reservationTime, Long location_vaccine_amount_id, Long user_id) {
         this.vaccinationTime = vaccinationTime;
         this.reservationTime = reservationTime;
         this.location_vaccine_amount_id = location_vaccine_amount_id;
+        this.user_id = user_id;
     }
 
     public Reservation() {
@@ -41,6 +44,10 @@ public class Reservation {
         return location_vaccine_amount_id;
     }
 
+    public Long getUser_id() {
+        return this.user_id;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -53,6 +60,10 @@ public class Reservation {
         this.location_vaccine_amount_id = location_vaccine_amount_id;
     }
 
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
     @Override
     public boolean equals(Object o){
         if (this == o) return true;
@@ -61,7 +72,8 @@ public class Reservation {
         Reservation r = (Reservation) o;
         return (this.id.equals(r.getId())
                 && this.vaccinationTime.equals(r.getVaccinationTime())
-                && this.location_vaccine_amount_id.equals(r.getLocation_vaccine_amount_id()));
+                && this.location_vaccine_amount_id.equals(r.getLocation_vaccine_amount_id()))
+                && this.user_id.equals(r.user_id);
     }
 
 

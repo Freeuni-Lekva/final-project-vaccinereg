26a1a0b0066977742dda5d52c0b15fdc3425a9fe
TM <tato.maisuradze@gmail.com>
2021-08-15 20:26:58 +0400

    Added vaccination center names to the table

--- VaccineReg/sql/vaccine_centers.sql
+++ VaccineReg/sql/vaccine_centers.sql
@@ -7,10 +7,14 @@ CREATE TABLE vaccine_centers (
     region_name CHAR(64) NOT NULL,
     city_name CHAR(64) NOT NULL,
     district_name CHAR(64) NOT NULL,
+    center_name CHAR(64) NOT NULL,
     people_limit_per_vaccine_at_same_time INT
 );
 
+
+
 INSERT INTO vaccine_centers VALUES
-(1, "Tbilisi", "Tbilisi", "Saburtalo", 400),
-(2, "Batumi", "Adjara", "idk", 350),
-(3, "Kutaisi", "Imereti", "idk2", 300);
+    (1, "Tbilisi", "Tbilisi", "Saburtalo", "Respublikuri", 400),
+    (2, "Adjara", "Batumi", "idk", "Batumi Respublikuri", 350),
+    (3, "Imereti", "Kutaisi", "idk2", "Kutaisi Respublikuri", 300);
+
